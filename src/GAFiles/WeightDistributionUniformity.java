954725990dfd3f07469af53ab3a391b5e0e0a934
Pk <Pk@Pk-PC>
2012-04-24 22:19:44 +0800

    Initial Commit

---
+++ src/GAFiles/WeightDistributionUniformity.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GAFiles;
+
+import org.jgap.Chromosome;
+import org.jgap.FitnessFunction;
+import org.jgap.IChromosome;
+
+/**
+ *
+ * @author Pk
+ */
+
+public class WeightDistributionUniformity extends FitnessFunction{
+
+    @Override
+    protected double evaluate(IChromosome ic) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+    
+}
