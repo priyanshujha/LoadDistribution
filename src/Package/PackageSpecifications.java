41e8decd0e7f0b58a67f1afa261fc31d2cc6cd95
Pk <Pk@Pk-PC>
2012-06-01 15:14:35 +0800

    Done!!!

---
+++ src/Package/PackageSpecifications.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package;
+
+/**
+ *
+ * @author Pk
+ */
+public class PackageSpecifications {
+    private int id;
+    private int wt;
+    private int length;
+    private int safetyFactor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getSafetyFactor() {
+        return safetyFactor;
+    }
+
+    public void setSafetyFactor(int safetyFactor) {
+        this.safetyFactor = safetyFactor;
+    }
+
+    public int getWt() {
+        return wt;
+    }
+
+    public void setWt(int wt) {
+        this.wt = wt;
+    }
+}
